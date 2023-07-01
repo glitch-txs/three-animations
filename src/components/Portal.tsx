4854a43be9ae91c07ad9e4b7fb231fa1412173c0
Glitch <glitch.txs@gmail.com>
2023-06-30 21:37:35 -0300

    add rome formater

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,32 +1,36 @@
-import { OrbitControls, useTexture, Environment, MeshPortalMaterial, RoundedBox } from "@react-three/drei";
-import * as THREE from 'three'
+import {
+	OrbitControls,
+	useTexture,
+	Environment,
+	MeshPortalMaterial,
+	RoundedBox,
+} from "@react-three/drei";
+import * as THREE from "three";
 
 type Props = {
-  texture: string
-}
+	texture: string;
+};
 
-const Portal = ({texture}: Props) => {
-  
-  const map = useTexture(texture)
+const Portal = ({ texture }: Props) => {
+	const map = useTexture(texture);
 
-  return (
-    <>
-    <ambientLight intensity={0.5}/>
-    <Environment preset="sunset" />
-      <OrbitControls />
-      <RoundedBox args={[2,3,0.1]} >
-        <MeshPortalMaterial side={THREE.DoubleSide} >
-          <ambientLight intensity={1}/>
-          <Environment preset="sunset" />
-          <mesh>
-            <sphereGeometry args={[5,64,64]}/>
-            <meshStandardMaterial map={map} side={THREE.BackSide} />
-          </mesh>
-        </MeshPortalMaterial>
-      </RoundedBox>
+	return (
+		<>
+			<ambientLight intensity={0.5} />
+			<Environment preset="sunset" />
+			<OrbitControls />
+			<RoundedBox args={[2, 3, 0.1]}>
+				<MeshPortalMaterial side={THREE.DoubleSide}>
+					<ambientLight intensity={1} />
+					<Environment preset="sunset" />
+					<mesh>
+						<sphereGeometry args={[5, 64, 64]} />
+						<meshStandardMaterial map={map} side={THREE.BackSide} />
+					</mesh>
+				</MeshPortalMaterial>
+			</RoundedBox>
+		</>
+	);
+};
 
-    </>
-  );
-}
-
-export default Portal
+export default Portal;
